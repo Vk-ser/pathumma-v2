c9755c0fec3a6fa8ba97e5668125383a803c5821
VK SER <89385674+Vk-ser@users.noreply.github.com>
2022-04-13 13:08:45 +0530

    VK SER

--- message/main.js
+++ message/main.js
@@ -1,6 +1,6 @@
 /* 
 Don't change credits🥲🚶
-Sourcecode https://github.com/VKsir/pathumma-v2
+Sourcecode https://github.com/Vk-sir/pathumma-v2
 Want to recode? yes, it's up to you as long as you don't change the watermark
 Don't sell the SC 😐👌
 */
